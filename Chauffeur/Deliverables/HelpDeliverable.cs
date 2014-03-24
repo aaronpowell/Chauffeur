177bdf8aec95dde44fc19f9df667c325181d2846
Aaron Powell <me@aaron-powell.com>
2014-03-24 18:46:51 +1100

    Improving help system and creating help for help

--- Chauffeur/Deliverables/HelpDeliverable.cs
+++ Chauffeur/Deliverables/HelpDeliverable.cs
@@ -10,7 +10,7 @@ namespace Chauffeur.Deliverables
     [DeliverableName("help")]
     [DeliverableAlias("h")]
     [DeliverableAlias("?")]
-    public class HelpDeliverable : Deliverable
+    public class HelpDeliverable : Deliverable, IProvideDirections
     {
         public HelpDeliverable(TextReader reader, TextWriter writer)
             : base(reader, writer)
@@ -19,18 +19,61 @@ namespace Chauffeur.Deliverables
 
         public override async Task<DeliverableResponse> Run(string[] args)
         {
-            var ipd = typeof(IProvideDirections);
             var deliverables = TypeFinder
-                .FindClassesOfType<Deliverable>()
-                .Where(t => ipd.IsAssignableFrom(t));
+                .FindClassesOfType<Deliverable>();
 
-            foreach (var deliverable in deliverables)
+            if (args.Any())
+                await Print(deliverables, args[0]);
+            else
+                await PrintAll(deliverables);
+
+            return DeliverableResponse.Continue;
+        }
+
+        private async Task Print(IEnumerable<Type> deliverables, string command)
+        {
+            var ipd = typeof(IProvideDirections);
+            var deliverable = deliverables
+                .Where(t => ipd.IsAssignableFrom(t))
+                .FirstOrDefault(t => t.GetCustomAttribute<DeliverableNameAttribute>(false).Name == command);
+
+            if (deliverable != null)
             {
                 var instance = (IProvideDirections)Activator.CreateInstance(deliverable, new object[] { In, Out });
                 await instance.Directions();
+                return;
             }
 
-            return DeliverableResponse.Continue;
+            await Out.WriteLineFormattedAsync(
+                "The command '{0}' doesn't implement help, you best contact the author",
+                command
+            );
+
+        }
+
+        private async Task PrintAll(IEnumerable<Type> deliverables)
+        {
+            await Out.WriteLineAsync("The following deliverables are loaded. Use `help <deliverable>` for detailed help");
+
+            foreach (var deliverable in deliverables)
+            {
+                var name = deliverable.GetCustomAttribute<DeliverableNameAttribute>(false).Name;
+                var aliases = deliverable.GetCustomAttributes<DeliverableAliasAttribute>(false).Select(a => a.Alias);
+
+                await Out.WriteLineFormattedAsync(
+                    "{0}{1}",
+                    name,
+                    aliases.Any() ? string.Format(" (aliases: {0})", string.Join(", ", aliases)) : string.Empty
+                );
+            }
+        }
+
+        public async Task Directions()
+        {
+            await Out.WriteLineAsync("help");
+            await Out.WriteLineAsync("\taliases: h, ?");
+            await Out.WriteLineAsync("\tUse `help` to display system help");
+            await Out.WriteLineAsync("\tUse `help <Deliverable>` to display help for a deliverable");
         }
     }
 }
