1d0e8faab9fdd9ad4ba62db966e5826b7e6d5f7e
Aaron Powell <me@aaron-powell.com>
2014-04-01 12:59:26 +1100

    New Delivery deliverable

---
+++ Chauffeur/Deliverables/DeliveryDeliverable.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+namespace Chauffeur.Deliverables
+{
+    [DeliverableName("delivery")]
+    [DeliverableAlias("d")]
+    public sealed class DeliveryDeliverable : Deliverable
+    {
+        public DeliveryDeliverable(TextReader reader, TextWriter writer)
+            : base(reader, writer)
+        {
+        }
+    }
+}
