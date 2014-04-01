d93689671158d40835e1411c7ade9a220ac34dc6
Aaron Powell <me@aaron-powell.com>
2014-04-01 13:08:47 +1100

    Starting run implementation, handling no packages

---
+++ Chauffeur.Tests/Deliverables/DeliveryDeliverableTests.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Chauffeur.Deliverables;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Chauffeur.Tests.Deliverables
+{
+    [TestFixture]
+    public class DeliveryDeliverableTests
+    {
+        [Test]
+        public async Task NoDeliveriesAbortsEarly()
+        {
+            var writer = Substitute.ForPartsOf<TextWriter>();
+            var delivery = new DeliveryDeliverable(null, writer);
+
+            await delivery.Run(null, new string[0]);
+
+            writer.Received(1).WriteLineAsync(Arg.Any<string>());
+        }
+    }
+}
--- Chauffeur/Deliverables/DeliveryDeliverable.cs
+++ Chauffeur/Deliverables/DeliveryDeliverable.cs
@@ -1,4 +1,7 @@
 ﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace Chauffeur.Deliverables
 {
     [DeliverableName("delivery")]
@@ -8,6 +11,18 @@ namespace Chauffeur.Deliverables
         public DeliveryDeliverable(TextReader reader, TextWriter writer)
             : base(reader, writer)
         {
+        }
+
+        public override async Task<DeliverableResponse> Run(string command, string[] args)
+        {
+            if (!args.Any())
+            {
+                await Out.WriteLineAsync("No deliveries were provided, use `help delivery` to see usage");
+                return DeliverableResponse.Continue;
+            }
+
+            await Out.WriteLineAsync(@"¯\_(ツ)_/¯");
+            return DeliverableResponse.Continue;
         }
     }
 }
