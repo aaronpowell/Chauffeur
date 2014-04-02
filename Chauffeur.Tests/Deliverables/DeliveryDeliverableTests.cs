1e421cdd974ea7d5a235e3e0e89cb2255247a5e7
Aaron Powell <me@aaron-powell.com>
2014-04-02 22:54:03 +1100

    Implementing handling when file doesn't exist

--- Chauffeur.Tests/Deliverables/DeliveryDeliverableTests.cs
+++ Chauffeur.Tests/Deliverables/DeliveryDeliverableTests.cs
@@ -1,6 +1,10 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions.TestingHelpers;
+using System.Linq;
 using System.Threading.Tasks;
 using Chauffeur.Deliverables;
+using Chauffeur.Host;
 using NSubstitute;
 using NUnit.Framework;
 
@@ -13,11 +17,47 @@ namespace Chauffeur.Tests.Deliverables
         public async Task NoDeliveriesAbortsEarly()
         {
             var writer = Substitute.ForPartsOf<TextWriter>();
-            var delivery = new DeliveryDeliverable(null, writer);
+            var delivery = new DeliveryDeliverable(null, writer, null, null);
 
             await delivery.Run(null, new string[0]);
 
             writer.Received(1).WriteLineAsync(Arg.Any<string>());
+        }
+
+        [Test]
+        public async Task NotFoundDeliveryAbortsEarly()
+        {
+            var writer = new MockTextWriter();
+            var settings = Substitute.For<IChauffeurSettings>();
+            string dir;
+            settings.TryGetChauffeurDirectory(out dir).Returns(x => {
+                x[0] = "";
+                return true;
+            });
+            var delivery = new DeliveryDeliverable(null, writer, new MockFileSystem(), settings);
+
+            await delivery.Run(null, new[] { "Test" });
+
+            Assert.That(writer.Messages.Count(), Is.EqualTo(1));
+        }
+
+        private class MockTextWriter : TextWriter
+        {
+            private readonly List<string> messages;
+            public MockTextWriter()
+            {
+                messages = new List<string>();
+            }
+
+            public IEnumerable<string> Messages { get { return messages; } }
+
+            public override System.Text.Encoding Encoding { get { return System.Text.Encoding.Default; } }
+
+            public override async Task WriteLineAsync(string value)
+            {
+                messages.Add(value);
+                await Task.FromResult(value);
+            }
         }
     }
 }
--- Chauffeur/Deliverables/DeliveryDeliverable.cs
+++ Chauffeur/Deliverables/DeliveryDeliverable.cs
@@ -1,6 +1,9 @@
 ﻿using System.IO;
+using System.IO.Abstractions;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using Chauffeur.Host;
 
 namespace Chauffeur.Deliverables
 {
@@ -8,9 +11,17 @@ namespace Chauffeur.Deliverables
     [DeliverableAlias("d")]
     public sealed class DeliveryDeliverable : Deliverable
     {
-        public DeliveryDeliverable(TextReader reader, TextWriter writer)
+        private readonly IFileSystem fileSystem;
+        private readonly IChauffeurSettings settings;
+        public DeliveryDeliverable(
+            TextReader reader,
+            TextWriter writer,
+            IFileSystem fileSystem,
+            IChauffeurSettings settings)
             : base(reader, writer)
         {
+            this.fileSystem = fileSystem;
+            this.settings = settings;
         }
 
         public override async Task<DeliverableResponse> Run(string command, string[] args)
@@ -21,8 +32,26 @@ namespace Chauffeur.Deliverables
                 return DeliverableResponse.Continue;
             }
 
-            await Out.WriteLineAsync(@"¯\_(ツ)_/¯");
+            string chauffeurFolder;
+            if (!settings.TryGetChauffeurDirectory(out chauffeurFolder))
+                return DeliverableResponse.Continue;
+
+            var tasks = args.Select(arg => Deliver(arg, chauffeurFolder));
+            await Task.WhenAll(tasks);
+
             return DeliverableResponse.Continue;
+        }
+
+        private async Task Deliver(string name, string chauffeurFolder)
+        {
+            var fileLocation = fileSystem.Path.Combine(chauffeurFolder, name + ".xml");
+            if (!fileSystem.File.Exists(fileLocation))
+            {
+                await Out.WriteLineFormattedAsync("The delivery '{0}' is not found in the Chauffeur folder", name);
+                return;
+            }
+
+            await Out.WriteLineAsync(@"¯\_(ツ)_/¯");
         }
     }
 }
