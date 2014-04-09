53adf5eff862441452a7e5c34922e2d77ee057a2
Aaron Powell <me@aaron-powell.com>
2014-04-09 13:51:31 +1000

    Sharing some base assembly info

--- Chauffeur/Properties/AssemblyInfo.cs
+++ Chauffeur/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -8,29 +7,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Chauffeur")]
 [assembly: AssemblyDescription("Chauffeur is a tool for helping with delivering changes to an Umbraco instance.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Aaron Powell")]
-[assembly: AssemblyProduct("Chauffeur")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c02a9d58-2448-4798-83b9-56c19aeeaf9e")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
---
+++ SolutionInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Aaron Powell")]
+[assembly: AssemblyProduct("Chauffeur")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+
+[assembly: ComVisible(false)]
