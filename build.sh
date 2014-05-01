08436b157822b326bf863a810664e231e32521be
Aaron Powell <me@aaron-powell.com>
2014-05-01 22:09:35 +1000

    Making it basically the same as the FAKE stuff itself

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 mono --runtime=v4.0 tools/nuget.exe install FAKE -OutputDirectory tools -ExcludeVersion
 mono --runtime=v4.0 tools/nuget.exe install nunit.runners -OutputDirectory tools -ExcludeVersion 
