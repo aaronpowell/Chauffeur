5b40d43f5a4a982fb543f477f15669cba1b94624
Aaron Powell <me@aaron-powell.com>
2014-05-01 21:23:35 +1000

    probably need to read my code a bit better...

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ cd tools
 wget http://nuget.org/nuget.exe
 cd ..
 
-mono --runtime=v4.0 tools/NuGet/nuget.exe install FAKE -OutputDirectory tools -ExcludeVersion
-mono --runtime=v4.0 tools/NuGet/nuget.exe install nunit.runner -OutputDirectory tools -ExcludeVersion 
+mono --runtime=v4.0 tools/nuget.exe install FAKE -OutputDirectory tools -ExcludeVersion
+mono --runtime=v4.0 tools/nuget.exe install nunit.runner -OutputDirectory tools -ExcludeVersion 
 mono --runtime=v4.0 tools/FAKE/tools/FAKE.exe build.fsx $@
