a15040d63fd3dfc4eeefaface19665f1354a13c1
Aaron Powell <me@aaron-powell.com>
2014-05-01 20:43:47 +1000

    Seeing if I can get builds via travis-ci

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget tools/nuget.exe http://nuget.org/nuget.exe
+
+mono --runtime=v4.0 tools/NuGet/nuget.exe install FAKE -OutputDirectory tools -ExcludeVersion
+mono --runtime=v4.0 tools/NuGet/nuget.exe install nunit.runner -OutputDirectory tools -ExcludeVersion 
+mono --runtime=v4.0 tools/FAKE/tools/FAKE.exe build.fsx $@
