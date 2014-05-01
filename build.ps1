39cd5017eef6923c41908503e7fb28f75bddf2b2
Aaron Powell <me@aaron-powell.com>
2014-05-01 22:03:09 +1000

    keeping NuGet in the repo

--- build.ps1
+++ build.ps1
@@ -3,18 +3,7 @@ param($Publish)
 $toolsDir = "tools"
 $nuget = "$toolsDir\nuget.exe"
 
-if (!(Test-Path $toolsDir)) {
-    New-Item $toolsDir -ItemType "Directory"
-}
-
-if (!(Test-Path $nuget)) {
-    "NuGet not installed, installing"
-
-    $wc = New-Object System.Net.WebClient
-    $wc.DownloadFile("http://nuget.org/nuget.exe", "$pwd\$nuget")
-}
-
-. $nuget "Install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion"
-. $nuget "Install" "nunit.runners" "-OutputDirectory" "tools" "-ExcludeVersion"
+. $nuget "Install" "FAKE" "-OutputDirectory" $toolsDir "-ExcludeVersion"
+. $nuget "Install" "nunit.runners" "-OutputDirectory" $toolsDir "-ExcludeVersion"
 
 . "$toolsDir\FAKE\tools\Fake.exe" "build.fsx"
--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env sh
 
-mkdir tools
-cd tools
-wget http://nuget.org/nuget.exe
-cd ..
-
 mono --runtime=v4.0 tools/nuget.exe install FAKE -OutputDirectory tools -ExcludeVersion
-mono --runtime=v4.0 tools/nuget.exe install nunit.runner -OutputDirectory tools -ExcludeVersion 
+mono --runtime=v4.0 tools/nuget.exe install nunit.runners -OutputDirectory tools -ExcludeVersion 
 mono --runtime=v4.0 tools/FAKE/tools/FAKE.exe build.fsx $@
