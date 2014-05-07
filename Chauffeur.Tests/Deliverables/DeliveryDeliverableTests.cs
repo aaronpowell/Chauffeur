8cb4fecc27867179e8abcac9f3276555ecab0c08
Aaron Powell <me@aaron-powell.com>
2014-05-07 22:48:03 +1000

    Starting work on #2, setting up a database table

---
+++ Chauffeur.Tests/Deliverables/DeliveryDeliverableTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chauffeur.Deliverables;
+using NSubstitute;
+using NUnit.Framework;
+using Umbraco.Core.Persistence;
+using Umbraco.Core.Persistence.DatabaseModelDefinitions;
+using Umbraco.Core.Persistence.SqlSyntax;
+
+namespace Chauffeur.Tests.Deliverables
+{
+    [TestFixture]
+    public class DeliveryDeliverableTests
+    {
+        [Test]
+        public async Task NoExistingDatabase_WillCreateTable()
+        {
+            SqlSyntaxContext.SqlSyntaxProvider = Substitute.For<ISqlSyntaxProvider>();
+            SqlSyntaxContext.SqlSyntaxProvider.Format(Arg.Any<ICollection<ForeignKeyDefinition>>()).Returns(new List<string>());
+            SqlSyntaxContext.SqlSyntaxProvider.Format(Arg.Any<ICollection<IndexDefinition>>()).Returns(new List<string>());
+
+            var conn = Substitute.For<IDbConnection>();
+            var db = new UmbracoDatabase(conn);
+
+            var deliverable = new DeliveryDeliverable(null, new MockTextWriter(), db);
+
+            await deliverable.Run(null, null);
+
+            Assert.Pass("No errors raised when creating the database");
+        }
+    }
+}
---
+++ Chauffeur/Deliverables/DeliveryDeliverable.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Umbraco.Core.Persistence;
+using Umbraco.Core.Persistence.DatabaseAnnotations;
+using Umbraco.Core.Persistence.SqlSyntax;
+namespace Chauffeur.Deliverables
+{
+    [DeliverableName("delivery")]
+    [DeliverableAlias("d")]
+    public sealed class DeliveryDeliverable : Deliverable
+    {
+        private readonly UmbracoDatabase database;
+
+        public const string TableName = "Chauffeur_Delivery";
+
+        public DeliveryDeliverable(
+            TextReader reader,
+            TextWriter writer,
+            UmbracoDatabase database
+        ) : base(reader, writer)
+        {
+            this.database = database;
+        }
+
+        public override async System.Threading.Tasks.Task<DeliverableResponse> Run(string command, string[] args)
+        {
+            if (!database.TableExist(TableName))
+            {
+                if (!await SetupDatabase())
+                {
+                    return DeliverableResponse.Continue;
+                }
+            }
+
+            return DeliverableResponse.Continue;
+        }
+
+        private async Task<bool> SetupDatabase()
+        {
+            await Out.WriteLineAsync("Chauffeur Delivery does not have its database setup. Setting up now.");
+
+            try
+            {
+                database.CreateTable<ChauffeurDeliveryTable>(true);
+            }
+            catch (Exception ex)
+            {
+                Out.WriteLine("Error creating the Chauffeur Delivery tracking table.");
+                Out.WriteLine(ex.ToString());
+                return false;
+            }
+
+            await Out.WriteLineAsync("Successfully created database table.");
+            return true;
+        }
+    }
+
+    [TableName(DeliveryDeliverable.TableName)]
+    [PrimaryKey("Id")]
+    class ChauffeurDeliveryTable
+    {
+        [Column("Id")]
+        [PrimaryKeyColumn(Name = "PK_id", IdentitySeed = 1)]
+        public int Id { get; set; }
+
+        [Column("Name")]
+        public string Name { get; set; }
+
+        [Column("ExecutionDate")]
+        public DateTimeOffset ExecutionDate { get; set; }
+
+        [Column("SignedFor")]
+        public bool SignedFor { get; set; }
+
+        [Column("Hash")]
+        public string Hash { get; set; }
+    }
+}
