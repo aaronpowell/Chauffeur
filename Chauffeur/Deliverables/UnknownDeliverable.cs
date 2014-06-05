a06f2503a2b05a135732a53d7caf12da7c3d1eca
Aaron Powell <me@aaron-powell.com>
2014-06-05 12:52:35 +1000

    Adding help for unknown

--- Chauffeur/Deliverables/UnknownDeliverable.cs
+++ Chauffeur/Deliverables/UnknownDeliverable.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 namespace Chauffeur.Deliverables
 {
     [DeliverableName("unknown")]
-    public sealed class UnknownDeliverable : Deliverable
+    public sealed class UnknownDeliverable : Deliverable, IProvideDirections
     {
         public UnknownDeliverable(TextReader reader, TextWriter writer)
             : base(reader, writer)
@@ -17,6 +17,11 @@ namespace Chauffeur.Deliverables
         {
             await Out.WriteLineFormattedAsync("Unknown command '{0}' entered, check `help` for available commands", string.Join(" ", new[] { command }.Concat(args)));
             return await base.Run(command, args);
+        }
+
+        public async Task Directions()
+        {
+            await Out.WriteLineAsync("Seriously, you're asking for help on the unknown command? Good luck with that");
         }
     }
 }
