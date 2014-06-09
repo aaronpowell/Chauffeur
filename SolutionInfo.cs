7fac40491aa3101b16db3edd77b5504e0adc7312
Aaron Powell <me@aaron-powell.com>
2014-06-09 14:52:44 +1000

    Bumping version

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.2.4.0")]
-[assembly: AssemblyFileVersion("0.2.4.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
 
 [assembly: ComVisible(false)]
