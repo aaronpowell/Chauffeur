c5a2ef65221513f4add86f16d159e7005a51fa0b
Aaron Powell <me@aaron-powell.com>
2015-06-03 15:52:15 +1000

    bumping a new version

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -3,11 +3,11 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Aaron Powell")]
 [assembly: AssemblyProduct("Chauffeur")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © 2014 - 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.3.1.0")]
-[assembly: AssemblyFileVersion("0.3.1.0")]
+[assembly: AssemblyVersion("0.3.2.0")]
+[assembly: AssemblyFileVersion("0.3.2.0")]
 
 [assembly: ComVisible(false)]
