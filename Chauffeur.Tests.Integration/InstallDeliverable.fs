efb72f439895becd006c9d9ba62233896a62ef80
Aaron Powell <me@aaron-powell.com>
2016-11-06 08:48:36 -0800

    Moving for a local setup method to a text fixture

--- Chauffeur.Tests.Integration/InstallDeliverable.fs
+++ Chauffeur.Tests.Integration/InstallDeliverable.fs
@@ -1,62 +1,40 @@
 ﻿module ``Install Deliverable``
-    open System.IO
-    open Chauffeur
-    open Chauffeur.Host
-    open Chauffeur.Tests.Integration
-    open Xunit
-    open FsUnit.Xunit
 
-    open TestHelpers
+open System.Data.SqlServerCe
+open Chauffeur
+open Chauffeur.Tests.Integration
+open Xunit
+open FsUnit.Xunit
+open TestHelpers
 
-    let setup() =
-        let dbFolder = setDataDirectory()
-
-        let writer = new MockTextWriter()
-        let reader = new MockTextReader()
-        let host = new UmbracoHost(reader, writer)
-
-        (dbFolder, host, writer, reader)
-
+[<CollectionAttribute("Basic host")>]
+type ``Install Deliverable Tests``(fixture : BasicHostCollectionFixture) = 
+    
     [<Fact>]
-    let ``Installation should result in a continuation when successful``() =
-        let dbFolder, host, writer, reader = setup()
-
-        reader.AddCommand "Y"
-
-        async {
-            do! writer.WriteLineAsync dbFolder |> Async.AwaitTask
-            let! response = host.Run([|"install"|]) |> Async.AwaitTask
-
-            writer.Dispose()
-            reader.Dispose()
-            host.Dispose()
-
+    member x.``Installation should result in a continuation when successful``() = 
+        fixture.TextReader.AddCommand "Y"
+        async { 
+            do! fixture.TextWriter.WriteLineAsync dbFolder |> Async.AwaitTask
+            let! response = fixture.Host.Run([| "install" |]) |> Async.AwaitTask
             response |> should equal DeliverableResponse.Continue
-        } |> Async.RunSynchronously
-
+        }
+        |> Async.RunSynchronously
+    
     [<Fact>]
-    let ``Installation should create a bunch of umbraco tables``() =
-        let dbFolder, host, writer, reader = setup()
-
-        reader.AddCommand "Y"
-
-        async {
-            do! writer.WriteLineAsync dbFolder |> Async.AwaitTask
-            let! response = host.Run([|"install"|]) |> Async.AwaitTask
-
-            writer.Dispose()
-            reader.Dispose()
-            host.Dispose()
-
+    member x.``Installation should create a bunch of umbraco tables``() = 
+        fixture.TextReader.AddCommand "Y"
+        async { 
+            do! fixture.TextWriter.WriteLineAsync dbFolder |> Async.AwaitTask
+            let! response = fixture.Host.Run([| "install" |]) |> Async.AwaitTask
             let connStrings = System.Configuration.ConfigurationManager.ConnectionStrings
-
-            use connection = new System.Data.SqlServerCe.SqlCeConnection(connStrings.["umbracoDbDSN"].ConnectionString)
-            let cmd =  new System.Data.SqlServerCe.SqlCeCommand("select table_name from information_schema.tables where TABLE_TYPE <> 'VIEW'", connection)
+            use connection = new SqlCeConnection(connStrings.["umbracoDbDSN"].ConnectionString)
+            let cmd = 
+                new SqlCeCommand("select table_name from information_schema.tables where TABLE_TYPE <> 'VIEW'", 
+                                 connection)
             connection.Open()
             let reader = cmd.ExecuteReader()
-
             while reader.Read() do
                 let tableName = reader.GetString 0
                 List.contains tableName knownTables |> should equal true
-
-        } |> Async.RunSynchronously
+        }
+        |> Async.RunSynchronously
