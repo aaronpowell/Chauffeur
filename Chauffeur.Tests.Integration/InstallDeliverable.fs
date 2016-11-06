5e5df58e31f2556610e1eb1df8221f7f4bdb7e24
Aaron Powell <me@aaron-powell.com>
2016-11-06 07:54:22 -0800

    Adding a test that asserts all the tables were created

--- Chauffeur.Tests.Integration/InstallDeliverable.fs
+++ Chauffeur.Tests.Integration/InstallDeliverable.fs
@@ -22,3 +22,28 @@
 
             response |> should equal DeliverableResponse.Continue
         } |> Async.RunSynchronously
+
+    [<Fact>]
+    let ``Installation should create a bunch of umbraco tables``() =
+        let dbFolder = setDataDirectory()
+
+        use writer = new MockTextWriter()
+        use reader = new MockTextReader(["Y"])
+        use host = new UmbracoHost(reader, writer)
+
+        async {
+            do! writer.WriteLineAsync dbFolder |> Async.AwaitTask
+            let! response = host.Run([|"install"|]) |> Async.AwaitTask
+
+            let connStrings = System.Configuration.ConfigurationManager.ConnectionStrings
+
+            use connection = new System.Data.SqlServerCe.SqlCeConnection(connStrings.["umbracoDbDSN"].ConnectionString)
+            let cmd =  new System.Data.SqlServerCe.SqlCeCommand("select table_name from information_schema.tables where TABLE_TYPE <> 'VIEW'", connection)
+            connection.Open()
+            let reader = cmd.ExecuteReader()
+
+            while reader.Read() do
+                let tableName = reader.GetString 0
+                List.contains tableName knownTables |> should equal true
+
+        } |> Async.RunSynchronously
--- Chauffeur.Tests.Integration/TestHelpers.fs
+++ Chauffeur.Tests.Integration/TestHelpers.fs
@@ -17,3 +17,51 @@ let setDataDirectory() =
     AppDomain.CurrentDomain.SetData("DataDirectory", folderForRun)
 
     folderForRun
+
+let knownTables =
+    ["cmsContent";
+    "cmsContentType";
+    "cmsContentType2ContentType";
+    "cmsContentTypeAllowedContentType";
+    "cmsContentVersion";
+    "cmsContentXml";
+    "cmsDataType";
+    "cmsDataTypePreValues";
+    "cmsDictionary";
+    "cmsDocument";
+    "cmsDocumentType";
+    "cmsLanguageText";
+    "cmsMacro";
+    "cmsMacroProperty";
+    "cmsMember";
+    "cmsMember2MemberGroup";
+    "cmsMemberType";
+    "cmsPreviewXml";
+    "cmsPropertyData";
+    "cmsPropertyType";
+    "cmsPropertyTypeGroup";
+    "cmsTagRelationship";
+    "cmsTags";
+    "cmsTask";
+    "cmsTaskType";
+    "cmsTemplate";
+    "umbracoAccess";
+    "umbracoAccessRule";
+    "umbracoCacheInstruction";
+    "umbracoDeployChecksum";
+    "umbracoDeployDependency";
+    "umbracoDomains";
+    "umbracoExternalLogin";
+    "umbracoLanguage";
+    "umbracoLog";
+    "umbracoMigration";
+    "umbracoNode";
+    "umbracoRedirectUrl";
+    "umbracoRelation";
+    "umbracoRelationType";
+    "umbracoServer";
+    "umbracoUser";
+    "umbracoUser2app";
+    "umbracoUser2NodeNotify";
+    "umbracoUser2NodePermission";
+    "umbracoUserType"]
