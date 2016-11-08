6723507d50d5cdd2da7581e0a997cff956887feb
Aaron Powell <me@aaron-powell.com>
2016-11-07 17:19:29 -0800

    going all in for F# syntax

--- Chauffeur.Tests.Integration/InstallDeliverable.fs
+++ Chauffeur.Tests.Integration/InstallDeliverable.fs
@@ -18,8 +18,12 @@ type ``Successfully setup the database``() =
     member x.``Results in a Continue response``() =
         x.TextReader.AddCommand "Y"
         async {
-            do! x.TextWriter.WriteLineAsync x.DatabaseLocation |> Async.AwaitTask
-            let! response = x.Host.Run([| "install" |]) |> Async.AwaitTask
+            do! x.DatabaseLocation
+                |> x.TextWriter.WriteLineAsync
+                |> Async.AwaitTask
+            let! response = [| "install" |]
+                            |> x.Host.Run
+                            |> Async.AwaitTask
             response |> should equal DeliverableResponse.Continue
         }
         |> Async.RunSynchronously
@@ -28,23 +32,32 @@ type ``Successfully setup the database``() =
     member x.``Creates known tables``() =
         x.TextReader.AddCommand "Y"
         async {
-            do! x.TextWriter.WriteLineAsync x.DatabaseLocation |> Async.AwaitTask
-            let! response = x.Host.Run([| "install" |]) |> Async.AwaitTask
+            do! x.DatabaseLocation
+                |> x.TextWriter.WriteLineAsync
+                |> Async.AwaitTask
+            let! response = [| "install" |]
+                            |> x.Host.Run
+                            |> Async.AwaitTask
             use connection = new SqlCeConnection(connStrings.["umbracoDbDSN"].ConnectionString)
             let cmd =
                 new SqlCeCommand("select table_name from information_schema.tables where TABLE_TYPE <> 'VIEW'",
                                  connection)
             connection.Open()
-            let reader = cmd.ExecuteReader()
-            while reader.Read() do
-                let tableName = reader.GetString 0
-                List.contains tableName knownTables |> should equal true
+
+            let rec testTable (reader: SqlCeDataReader) =
+                if reader.Read() then
+                    let tableName = reader.GetString 0
+                    List.contains tableName knownTables |> should equal true
+                    testTable reader
+                else
+                    ignore
+
+            cmd.ExecuteReader() |> testTable |> ignore
         }
         |> Async.RunSynchronously
 
 type ``Unsuccessfully setup the database``() =
     inherit UmbracoHostTestBase()
-
     [<Fact>]
     member x.``Won't create the database when you say not to``() =
         x.TextReader.AddCommand "N"
@@ -52,7 +65,6 @@ type ``Unsuccessfully setup the database``() =
             do! x.TextWriter.WriteLineAsync x.DatabaseLocation |> Async.AwaitTask
             let! response = x.Host.Run([| "install" |]) |> Async.AwaitTask
             use connection = new SqlCeConnection(connStrings.["umbracoDbDSN"].ConnectionString)
-
             connection.Open |> should throw typeof<SqlCeException>
         }
         |> Async.RunSynchronously
