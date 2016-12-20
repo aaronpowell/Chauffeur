9ba91bfdb4acdb848af0e04cd7f70b5b5aca45d1
Aaron Powell <me@aaron-powell.com>
2016-12-20 21:22:22 +1100

    adding some logging to the ps build script

--- build.ps1
+++ build.ps1
@@ -1,7 +1,10 @@
+'Build starting'
+
 $toolsDir = "tools"
 $nuget = "$toolsDir\nuget.exe"
 
 . $nuget "Install" "FAKE.Core" "-OutputDirectory" $toolsDir "-ExcludeVersion"
 . $nuget "Install" "xunit.runner.console" "-OutputDirectory" $toolsDir "-ExcludeVersion"
 
+'Dependencies downloaded, time to run FAKE'
 . "$toolsDir\FAKE.Core\tools\Fake.exe" "build.fsx"
