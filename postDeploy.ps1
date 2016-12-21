25c49671019ecb7c32b1a4c1c4c1a681da95c20a
Aaron Powell <me@aaron-powell.com>
2016-12-21 14:41:02 +1100

    removing after_deploy script

--- postDeploy.ps1
+++
@@ -1,6 +0,0 @@
-$toolsDir = "tools"
-$nuget = "$toolsDir\nuget.exe"
-
-$symbolServer = "http://nuget.gw.symbolsource.org/Public/NuGet"
-
-gci | ?{ $_.Name -match "^.*\.symbols\.nupkg" } | %{ $_.Name } | %{ . $nuget "Push" $_ "-Source" $symbolServer }
