a6af09100d66724c4a41ca7745e10b7c99b23eb5
Aaron Powell <me@aaron-powell.com>
2018-08-30 11:09:20 +1000

    WIP

---
+++ Chauffeur.Demo/CustomStartup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Umbraco.Core;
+
+namespace Chauffeur.Demo
+{
+    public class CustomStartup : ApplicationEventHandler
+    {
+        public CustomStartup()
+        {
+            UmbracoApplicationBase.ApplicationInit += AppInit;
+        }
+
+        private void AppInit(object sender, EventArgs e)
+        {
+        }
+    }
+}
