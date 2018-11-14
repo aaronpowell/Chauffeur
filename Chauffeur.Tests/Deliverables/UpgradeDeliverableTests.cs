ab9a986b902307420e6e02c381ae2c03818f2736
Steve Temple <steve@gibedigital.com>
2018-11-14 23:24:26 +0000

    Add upgrade deliverable

---
+++ Chauffeur.Tests/Deliverables/UpgradeDeliverableTests.cs
@@ -0,0 +1,25 @@
+﻿using Chauffeur.Deliverables;
+using NSubstitute;
+using Umbraco.Core.Services;
+using Xunit;
+using Task = System.Threading.Tasks.Task;
+
+namespace Chauffeur.Tests.Deliverables
+{
+    public class UpgradeDeliverableTests
+    {
+        [Fact]
+        public async Task NoMigrationEntries_WillWarnAndExit()
+        {
+            var writer = new MockTextWriter();
+	        var migrationEntryService = Substitute.For<IMigrationEntryService>();
+
+            var deliverable = new UpgradeDeliverable(null, writer, null, migrationEntryService);
+
+            var response = await deliverable.Run(null, null);
+
+            Assert.Single(writer.Messages);
+			Assert.Equal(DeliverableResponse.FinishedWithError,response);
+        }
+    }
+}
---
+++ Chauffeur/Deliverables/UpgradeDeliverable.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Semver;
+using Umbraco.Core;
+using Umbraco.Core.Configuration;
+using Umbraco.Core.Persistence.Migrations;
+using Umbraco.Core.Services;
+
+namespace Chauffeur.Deliverables
+{
+    [DeliverableName("upgrade")]
+    public sealed class UpgradeDeliverable : Deliverable
+    {
+        private readonly ApplicationContext appContext;
+	    private readonly IMigrationEntryService migrationEntryService;
+
+		public UpgradeDeliverable(
+            TextReader reader,
+            TextWriter writer,
+            ApplicationContext appContext,
+			IMigrationEntryService migrationEntryService
+            )
+            : base(reader, writer)
+        {
+            this.appContext = appContext;
+	        this.migrationEntryService = migrationEntryService;
+
+        }
+
+        public override async Task<DeliverableResponse> Run(string command, string[] args)
+        {
+	        try
+	        {
+		        var currentVersion = FindCurrentDbVersion();
+		        await Out.WriteLineAsync(
+			        $"Attempting to upgrade from {currentVersion} to {UmbracoVersion.GetSemanticVersion()}");
+
+		        var runner = new MigrationRunner(
+			        migrationEntryService,
+			        appContext.ProfilingLogger.Logger, 
+			        currentVersion, 
+			        UmbracoVersion.GetSemanticVersion(),
+			        Constants.System.UmbracoMigrationName);
+
+		        var upgraded = runner.Execute(appContext.DatabaseContext.Database, isUpgrade:true);
+
+		        if (upgraded == false)
+		        {
+			        await base.Out.WriteLineAsync("Upgrading failed, see log for full details");
+			        return DeliverableResponse.FinishedWithError;
+		        }
+
+		        await base.Out.WriteLineAsync("Upgrading completed");
+		        return DeliverableResponse.Continue;
+	        }
+	        catch (NoConfiguredVersionException)
+	        {
+		        await base.Out.WriteLineAsync("Can't upgrade as there is no configured version");
+		        return DeliverableResponse.FinishedWithError;
+	        }
+		}
+
+	    private SemVersion FindCurrentDbVersion()
+	    {
+		    var entries = migrationEntryService.GetAll(Constants.System.UmbracoMigrationName).ToArray();
+		    if (entries.Any())
+		    {
+			    return entries.OrderBy(e => e.Version).Last().Version;
+		    }
+			throw new NoConfiguredVersionException();
+	    }
+    }
+
+	public class NoConfiguredVersionException : Exception
+	{
+
+	}
+}
