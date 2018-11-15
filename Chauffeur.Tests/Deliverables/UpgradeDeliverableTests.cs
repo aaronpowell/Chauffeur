d421c692a99404936f635177c758f546a537edf3
Steve Temple <steve@gibedigital.com>
2018-11-15 00:12:19 +0000

    Refactored and added more unit tests

--- Chauffeur.Tests/Deliverables/UpgradeDeliverableTests.cs
+++ Chauffeur.Tests/Deliverables/UpgradeDeliverableTests.cs
@@ -1,5 +1,11 @@
-﻿using Chauffeur.Deliverables;
+﻿using System;
+using System.Linq;
+using Chauffeur.Deliverables;
 using NSubstitute;
+using Semver;
+using Umbraco.Core;
+using Umbraco.Core.Configuration;
+using Umbraco.Core.Models;
 using Umbraco.Core.Services;
 using Xunit;
 using Task = System.Threading.Tasks.Task;
@@ -11,15 +17,84 @@ namespace Chauffeur.Tests.Deliverables
         [Fact]
         public async Task NoMigrationEntries_WillWarnAndExit()
         {
-            var writer = new MockTextWriter();
-            var migrationEntryService = Substitute.For<IMigrationEntryService>();
-
-            var deliverable = new UpgradeDeliverable(null, writer, null, migrationEntryService);
+            var writer = Writer();
+            
+            var deliverable = new UpgradeDeliverable(null, writer, null, MigrationEntryService());
 
             var response = await deliverable.Run(null, null);
 
             Assert.Single(writer.Messages);
             Assert.Equal(DeliverableResponse.FinishedWithError, response);
+        }
+
+        [Fact]
+        public async Task SameVersions_NothingDone()
+        {
+            var writer = Writer();
+            
+            var deliverable = new UpgradeDeliverable(null, writer, null, MigrationEntryService(UmbracoVersion.GetSemanticVersion()));
+
+            var response = await deliverable.Run(null, null);
+
+            Assert.Single(writer.Messages);
+            Assert.Equal(DeliverableResponse.FinishedWithError, response);
+        }
+
+        [Fact]
+        public async Task CorrectSameVersionSelected_NothingDone()
+        {
+            var writer = Writer();
+            
+            var deliverable = new UpgradeDeliverable(null, writer, null, MigrationEntryService(new SemVersion(7, 1), UmbracoVersion.GetSemanticVersion(), new SemVersion(7,3)));
+
+            var response = await deliverable.Run(null, null);
+
+            Assert.Single(writer.Messages);
+            Assert.Equal(DeliverableResponse.FinishedWithError, response);
+        }
+
+        [Fact]
+        public async Task DifferentVersions_UpgradeIsCompleted()
+        {
+            var writer = Writer();
+            var deliverable = new UpgradeDeliverable(null, writer, Upgrader(true), MigrationEntryService(new SemVersion(7,1)));
+
+            var response = await deliverable.Run(null, null);
+
+            Assert.True(writer.Messages.Count() == 2);
+            Assert.Equal(DeliverableResponse.Continue, response);
+        }
+
+        [Fact]
+        public async Task UpgradeFailes_ErrorStateReturned()
+        {
+            var writer = Writer();
+            var deliverable = new UpgradeDeliverable(null, writer, Upgrader(false), MigrationEntryService(new SemVersion(7, 1)));
+
+            var response = await deliverable.Run(null, null);
+
+            Assert.True(writer.Messages.Count() == 2);
+            Assert.Equal(DeliverableResponse.FinishedWithError, response);
+        }
+
+        private MockTextWriter Writer()
+        {
+            return new MockTextWriter();
+        }
+        
+        private IMigrationEntryService MigrationEntryService(params SemVersion[] versions)
+        {
+            var migrationEntryService = Substitute.For<IMigrationEntryService>();
+            var entries = versions.Select(v => new MigrationEntry(1, DateTime.Now, Constants.System.UmbracoMigrationName, v));
+            migrationEntryService.GetAll(Constants.System.UmbracoMigrationName).Returns(entries);
+            return migrationEntryService;
+        }
+
+        private IUpgrader Upgrader(bool returns)
+        {
+            var upgrader = Substitute.For<IUpgrader>();
+            upgrader.Upgrade(new SemVersion(7, 1), UmbracoVersion.GetSemanticVersion()).Returns(returns);
+            return upgrader;
         }
     }
 }
--- Chauffeur/Deliverables/UpgradeDeliverable.cs
+++ Chauffeur/Deliverables/UpgradeDeliverable.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Semver;
 using Umbraco.Core;
+using Umbraco.Core.Logging;
 using Umbraco.Core.Configuration;
+using Umbraco.Core.Persistence;
 using Umbraco.Core.Persistence.Migrations;
 using Umbraco.Core.Services;
 
@@ -13,20 +15,19 @@ namespace Chauffeur.Deliverables
     [DeliverableName("upgrade")]
     public sealed class UpgradeDeliverable : Deliverable
     {
-        private readonly ApplicationContext appContext;
+        private readonly IUpgrader upgrader;
         private readonly IMigrationEntryService migrationEntryService;
 
         public UpgradeDeliverable(
             TextReader reader,
             TextWriter writer,
-            ApplicationContext appContext,
+            IUpgrader upgrader,
             IMigrationEntryService migrationEntryService
             )
             : base(reader, writer)
         {
-            this.appContext = appContext;
+            this.upgrader = upgrader;
             this.migrationEntryService = migrationEntryService;
-
         }
 
         public override async Task<DeliverableResponse> Run(string command, string[] args)
@@ -34,17 +35,16 @@ namespace Chauffeur.Deliverables
             try
             {
                 var currentVersion = FindCurrentDbVersion();
-                await Out.WriteLineAsync(
-                    $"Attempting to upgrade from {currentVersion} to {UmbracoVersion.GetSemanticVersion()}");
+                var targetVersion = UmbracoVersion.GetSemanticVersion();
 
-                var runner = new MigrationRunner(
-                    migrationEntryService,
-                    appContext.ProfilingLogger.Logger,
-                    currentVersion,
-                    UmbracoVersion.GetSemanticVersion(),
-                    Constants.System.UmbracoMigrationName);
+                if (currentVersion == targetVersion)
+                {
+                    await Out.WriteLineAsync($"Version is up to date {currentVersion} no work todo");
+                    return DeliverableResponse.FinishedWithError;
+                }
 
-                var upgraded = runner.Execute(appContext.DatabaseContext.Database, isUpgrade: true);
+                await Out.WriteLineAsync($"Upgrading from {currentVersion} to {targetVersion}");
+                var upgraded = upgrader.Upgrade(currentVersion, targetVersion);
 
                 if (upgraded == false)
                 {
@@ -76,5 +76,37 @@ namespace Chauffeur.Deliverables
     public class NoConfiguredVersionException : Exception
     {
 
+    }
+
+    public interface IUpgrader
+    {
+        bool Upgrade(SemVersion from, SemVersion to);
+    }
+
+    public class Upgrader : IUpgrader
+    {
+        
+        private ILogger logger;
+        private IMigrationEntryService migrationEntryService;
+        private UmbracoDatabase database;
+
+        public Upgrader(IMigrationEntryService migrationEntryService, ILogger logger, UmbracoDatabase database)
+        {
+            this.logger = logger;
+            this.migrationEntryService = migrationEntryService;
+            this.database = database;
+        }
+
+        public bool Upgrade(SemVersion from, SemVersion to)
+        {
+            var runner = new MigrationRunner(
+                migrationEntryService,
+                logger,
+                from,
+                to,
+                Constants.System.UmbracoMigrationName);
+
+            return runner.Execute(database, isUpgrade: true);
+        }
     }
 }
