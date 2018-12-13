6d3e9b2dcb6a383e17846e26df2fdc16ae710d5d
Aaron Powell <me@aaron-powell.com>
2018-12-14 09:02:51 +1100

    simplifying quit deliverable

--- src/Chauffeur/Deliverables/QuitDeliverable.fs
+++ src/Chauffeur/Deliverables/QuitDeliverable.fs
@@ -1,11 +1,11 @@
 ﻿namespace Chauffeur.Deliverables
 
 open Chauffeur
-open System.Threading.Tasks
+open FSharp.Control.Tasks.V2
 
 [<DeliverableName("quit")>]
 [<DeliverableAlias("q")>]
 type QuitDeliverable(reader, writer) =
     inherit Deliverable(reader, writer)
-    override __.Run _ _ = Task.FromResult(DeliverableResponse.Shutdown)
+    override __.Run _ _ = task { return DeliverableResponse.Shutdown }
 
