eadeaf569777e0470a0e41a70a9d904838f3fc62
Aaron Powell <me@aaron-powell.com>
2018-12-29 12:40:09 +1100

    Implementing the InstallDeliverable

--- src/Chauffeur/Deliverables/InstallDeliverable.fs
+++ src/Chauffeur/Deliverables/InstallDeliverable.fs
@@ -4,6 +4,11 @@ open System
 open Chauffeur
 open FSharp.Control.Tasks.V2
 open System.IO.Abstractions
+open System.Threading.Tasks
+
+type wla = string -> Task
+type rla = unit -> Task<string>
+type cdb = string -> unit
 
 module InstallDeliverable =
     open System.Configuration
@@ -14,7 +19,35 @@ module InstallDeliverable =
     let isSqlCe (connStr : ConnectionStringSettings) =
         connStr.ProviderName = "System.Data.SqlServerCe.4.0"
 
-    let createSqlCeDb dataDirectory pathCombine fileExists createDb (writeLineAsync : wla) (connStr : string) =
+    let prompt (writeAsync : wla) readLineAsync (message : string) defaultValue =
+        task {
+            do! writeAsync message
+            let! value = readLineAsync()
+            return match value with
+                   | value when value = String.Empty -> defaultValue
+                   | _ -> value
+        }
+
+    let internal makeDb writeLineAsync readLineAsync (createDb : unit -> unit) args =
+        task {
+            do! writeLineAsync "The SqlCE database specified in the connection string doesn't appear to exist."
+            let! response = match args |> Array.toList with
+                            | "y" :: _ | "Y" :: _ -> task { return "Y" }
+                            | _ -> prompt writeLineAsync readLineAsync "" "Y"
+
+            return! match response with
+                    | "Y" | "y" -> task {
+                        do! writeLineAsync "Creating the database"
+                        createDb()
+                        return true
+                     }
+                    | _ -> task {
+                        do! writeLineAsync "Installation is being aborted"
+                        return false
+                     }
+        }
+
+    let getSqlCePath dataDirectory pathCombine (connStr : string) =
         let split c (s : string) = s.Split(c)
         let trim (s : string) = s.Trim()
 
@@ -28,13 +61,22 @@ module InstallDeliverable =
                          |> Seq.last
                          |> trim
 
-        match fileExists (pathCombine [|dataDirectory;dbFileName|]) with
-        | false ->
-            task {
-                do! writeLineAsync "The SqlCE database specified in the connection string doesn't appear to exist."
-                createDb connStr
-            }
-        | _ -> task { return ignore() }
+        pathCombine [|dataDirectory;dbFileName|]
+
+    let createSqlCeDb (readLineAsync : rla)
+                      fileExists
+                      (createDb : unit -> unit)
+                      (writeLineAsync : wla)
+                      args
+                      dbPath =
+        match fileExists dbPath with
+        | true -> task { return Some(dbPath) }
+        | false -> task {
+            let! result = makeDb writeLineAsync readLineAsync createDb args
+            match result with
+            | true -> return Some(dbPath)
+            | false -> return None
+        }
 
 open Umbraco.Core.Migrations.Install
 open InstallDeliverable
@@ -52,13 +94,12 @@ type InstallDeliverable
     let connStr = settings.ConnectionString
 
     let createSqlDb' = createSqlCeDb
-                        (AppDomain.CurrentDomain.GetData("DataDirectory") :?> string)
-                        fileSystem.Path.Combine
+                        reader.ReadLineAsync
                         fileSystem.File.Exists
-                        sqlCeFactory.CreateDatabase
+                        (fun () -> sqlCeFactory.CreateDatabase connStr.ConnectionString)
                         writer.WriteLineAsync
 
-    override __.Run _ _ =
+    override __.Run _ args =
         task {
             match connStrExists connStr with
             | true ->
@@ -67,12 +108,18 @@ type InstallDeliverable
             | false ->
                 match isSqlCe connStr with
                 | true ->
-                    do! createSqlDb' connStr.ConnectionString
+                    let dbPath = getSqlCePath (AppDomain.CurrentDomain.GetData("DataDirectory") :?> string) fileSystem.Path.Combine connStr.ConnectionString
+                    let! _ = createSqlDb' args dbPath
+                    ignore()
                 | false ->
                     ignore()
 
-                // databaseBuilder.CreateSchemaAndData()
+                let result = databaseBuilder.CreateSchemaAndData()
 
-                return DeliverableResponse.Continue
+                match result.Success with
+                | true -> return DeliverableResponse.Continue
+                | false ->
+                    do! writer.WriteLineAsync(sprintf "Failed to install db: %s" result.Message)
+                    return DeliverableResponse.FinishedWithError
         }
         
