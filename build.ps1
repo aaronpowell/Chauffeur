87ea013ff86870a3e9207c2e95f8c66b27d21a9c
Aaron Powell <me@aaron-powell.com>
2019-03-01 06:14:44 +1000

    was unpacking codecov to the wrong place

--- build.ps1
+++ build.ps1
@@ -15,7 +15,7 @@ if ($Target -eq 'Setup') {
     dotnet tool install coverlet.console --tool-path ./$fakeDir
 
     (New-Object System.Net.WebClient).DownloadFile("https://github.com/codecov/codecov-exe/releases/download/1.2.0/Codecov.zip", (Join-Path $pwd "Codecov.zip")) # Download Codecov.zip from github release.
-    Expand-Archive .\Codecov.zip -DestinationPath ./codecov/$fakeDir
+    Expand-Archive .\Codecov.zip -DestinationPath ./$fakeDir/codecov
     Remove-Item .\Codecov.zip
 } else {
     . "$fakeDir/fake.exe" run ./build.fsx target $Target
