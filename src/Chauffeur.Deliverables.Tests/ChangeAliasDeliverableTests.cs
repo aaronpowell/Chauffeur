a1efb5f835f33dd55054bad4d6ce21c7fd4291fe
Aaron Powell <me@aaron-powell.com>
2019-03-25 09:37:43 +1100

    Bringing in the Change Alias deliverable

---
+++ src/Chauffeur.Deliverables.Tests/ChangeAliasDeliverableTests.cs
@@ -0,0 +1,66 @@
+﻿using NSubstitute;
+using Umbraco.Core.Models;
+using Umbraco.Core.Services;
+using Task = System.Threading.Tasks.Task;
+using Xunit;
+
+namespace Chauffeur.Deliverables.Tests
+{
+    public class ChangeAliasDeliverableTests
+    {
+        [Fact]
+        public async Task NoArguments_WillWarnAndExit()
+        {
+            var writer = new MockTextWriter();
+            var delivery = new ChangeAliasDeliverable(writer, null, null);
+
+            await delivery.Run(null, new string[0]);
+
+            Assert.Single(writer.Messages);
+        }
+
+        [Fact]
+        public async Task WhatOnly_WillWarnAndExit()
+        {
+            var writer = new MockTextWriter();
+            var delivery = new ChangeAliasDeliverable(writer, null, null);
+
+            await delivery.Run(null, new[] { "dt" });
+
+            Assert.Single(writer.Messages);
+        }
+
+        [Fact]
+        public async Task MissingNewAlias_WillWarnAndExit()
+        {
+            var writer = new MockTextWriter();
+            var delivery = new ChangeAliasDeliverable(writer, null, null);
+
+            await delivery.Run(null, new[] { "dt", "old" });
+
+            Assert.Single(writer.Messages);
+        }
+
+        [Theory]
+        [InlineData("document-type")]
+        [InlineData("doc-type")]
+        [InlineData("dt")]
+        public async Task ProvidingValidDocTypeAlias_WillUpdateToNewAlias(string what)
+        {
+            var writer = new MockTextWriter();
+
+            var cts = Substitute.For<IContentTypeService>();
+            const string old = "old";
+            const string @new = "new";
+            var result = Substitute.For<IContentType>();
+            result.Alias = old;
+            cts.Get(Arg.Is(old)).Returns(result);
+
+            var deliverable = new ChangeAliasDeliverable(writer, null, cts);
+
+            await deliverable.Run(null, new[] { what, old, @new });
+
+            Assert.Equal(result.Alias, @new);
+        }
+    }
+}
---
+++ src/Chauffeur.Deliverables/ChangeAliasDeliverable.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Umbraco.Core.Services;
+
+namespace Chauffeur.Deliverables
+{
+    [DeliverableName("change-alias")]
+    [DeliverableAlias("ca")]
+    public sealed class ChangeAliasDeliverable : Deliverable
+    {
+        private readonly IContentTypeService contentTypeService;
+
+        public ChangeAliasDeliverable(
+            TextWriter writer,
+            TextReader reader,
+            IContentTypeService contentTypeService
+            )
+            : base(reader, writer)
+        {
+            this.contentTypeService = contentTypeService;
+        }
+
+        public override async Task<DeliverableResponse> Run(string command, string[] args)
+        {
+            if (args.Length != 3)
+            {
+                await Out.WriteLineAsync("Invalid arguments, expected format of `change-alias <what> <old> <new>`");
+                return DeliverableResponse.Continue;
+            }
+
+            var what = args[0].ToLower();
+
+            var old = args[1];
+            var @new = args[2];
+
+            switch (what)
+            {
+                case "document-type":
+                case "doc-type":
+                case "dt":
+                    var ct = contentTypeService.Get(old);
+                    ct.Alias = @new;
+                    contentTypeService.Save(ct);
+                    break;
+
+                default:
+                    await Out.WriteLineAsync($"Presently we cannot change the alias of `{what}`. Run `help ca` to see what can be changed");
+                    break;
+            }
+
+            return DeliverableResponse.Continue;
+        }
+    }
+}
