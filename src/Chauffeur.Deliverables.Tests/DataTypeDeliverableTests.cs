742318510aa4e1d3662361901bd057bf8d892b88
Aaron Powell <me@aaron-powell.com>
2019-04-16 10:52:00 +1000

    bringing in the data type deliverable

---
+++ src/Chauffeur.Deliverables.Tests/DataTypeDeliverableTests.cs
@@ -0,0 +1,52 @@
+﻿using NSubstitute;
+using System.Collections.Generic;
+using System.IO.Abstractions.TestingHelpers;
+using Umbraco.Core.Models;
+using Umbraco.Core.Services;
+using Xunit;
+
+namespace Chauffeur.Deliverables.Tests
+{
+    public class DataTypeDeliverableTests
+    {
+        [Fact]
+        public async System.Threading.Tasks.Task CanExportAllDataTypeDefinitions()
+        {
+            var writer = new MockTextWriter();
+
+            var defs = new List<IDataType>();
+            var d = Substitute.For<IDataType>();
+            d.Id = 1;
+            d.Name = "One";
+            defs.Add(d);
+
+            d = Substitute.For<IDataType>();
+            d.Id = 2;
+            d.Name = "Two";
+            defs.Add(d);
+
+            var dataTypeService = Substitute.For<IDataTypeService>();
+            dataTypeService.GetAll(Arg.Any<int>()).Returns(defs);
+
+            var fs = new MockFileSystem();
+
+            var settings = Substitute.For<IChauffeurSettings>();
+            settings.TryGetChauffeurDirectory(out string dir).Returns(x =>
+             {
+                 x[0] = "c:\\chauffeur";
+                 return true;
+             });
+            fs.AddDirectory("c:\\chauffeur");
+
+            var serializer = Substitute.For<IEntityXmlSerializer>();
+            serializer.Serialize(Arg.Any<IEnumerable<IDataType>>())
+                .Returns(new System.Xml.Linq.XElement("Foo"));
+
+            var deliverable = new DataTypeDeliverable(null, writer, dataTypeService, fs, settings, serializer);
+
+            await deliverable.Run("data-type", new[] { "export" });
+
+            Assert.Single(fs.AllFiles);
+        }
+    }
+}
